d7c4e8f4c82504ae60a33c015050fd80a6476178
MimiMagusin <mimi_magusin@hotmail.com>
2017-10-09 17:37:00 +0200

    Set Up Client to seed data iAPI, install missing packages, add the seed command to package.json

--- src/seeds.js
+++ src/seeds.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-console */
+const feathers = require('feathers/client');
+const rest = require('feathers-rest/client');
+const superagent = require('superagent');
+const hooks = require('feathers-hooks');
+const auth = require('feathers-authentication-client');
+
 const user = {
   email: 'teacher@teacher.dev',
   password: 'abcd1234'
@@ -19,3 +26,22 @@ const user = {
 //     totalScore: [],
 //   }
 // ]
+
+const feathersClient = feathers();
+
+feathersClient
+  .configure(hooks())
+  .configure(rest('http://localhost:3030').superagent(superagent))
+  .configure(auth());
+
+feathersClient.service('users').create(user)
+  .then(() => {
+    feathersClient.authenticate({
+      strategy: 'local',
+      email: user.email,
+      password: user.password
+    });
+  })
+  .catch(function(error) {
+    console.error('Error creating user!', error);
+  });
