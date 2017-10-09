6a8ab4184d50afb1c1d720e86c5075b1c51dba4e
MimiMagusin <mimi_magusin@hotmail.com>
2017-10-09 17:07:09 +0200

    Generate feathers authentication

--- src/models/batches.model.js
+++ src/models/batches.model.js
@@ -8,25 +8,25 @@ module.exports = function (app) {
 
   const evaluations = new Schema ({
     color: { type: String },
-    remark: { type: Text },
+    remark: { type: String },
     date: { type: Date, default: Date.now},
-  })
+  });
 
   const students = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     profilePicture: { type: String, required: true },
-    evaluation: [evaluationSchema],
+    evaluation: [evaluations],
     batch: { type: String},
     currentEvaluation: { type: String},
-  })
+  });
 
 
   const batches = new Schema({
     number: { type: Number, required: true },
     startDate: { type: Date },
     endDate: { type: Date },
-    students: [studentSchema],
+    students: [students],
     totalScore: [{ type: String }],
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
@@ -34,27 +34,3 @@ module.exports = function (app) {
 
   return mongooseClient.model('batches', batches);
 };
-
-
-module.exports = function (app) {
-  const mongooseClient = app.get('mongooseClient');
-
-  const playerSchema = new mongooseClient.Schema({
-    userId: { type: mongooseClient.Schema.Types.ObjectId, ref: 'users' },
-    points: [{ type: Number, default: 0 }],
-
-    riddle: [{ type: mongooseClient.Schema.Types.ObjectId, ref: 'riddles' }],
-    question: [{ type: String }],
-    won: [{ type: Boolean }]
-  });
-
-  const games = new mongooseClient.Schema({
-    title: { type: String, required: true },
-    winnerId: { type: mongooseClient.Schema.Types.ObjectId, ref: 'users' },
-    started: { type: Boolean, default: false },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-    players: [ playerSchema ],
-  });
-  games.loadClass(GameClass);
-  return mongooseClient.model('games', games);
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const today = require('./today/today.service.js');
 const users = require('./users/users.service.js');
 const batches = require('./batches/batches.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(today);
   app.configure(users);
   app.configure(batches);
+  app.configure(users);
 };
