0d47ed6a23c83f8a0163c06f1b8deb355a15e9f2
MimiMagusin <mimi_magusin@hotmail.com>
2017-10-09 16:46:33 +0200

    Set up users Schema, update according to recipe schema

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const today = require('./today/today.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(today);
+  app.configure(users);
 };
