04ef05789fdbe90f180a507b8cb928d4ff02b154
MimiMagusin <mimi_magusin@hotmail.com>
2017-10-09 17:25:14 +0200

    Set up Super User in seeds file

---
+++ src/seeds.js
@@ -0,0 +1,21 @@
+const user = {
+  email: 'teacher@teacher.dev',
+  password: 'abcd1234'
+};
+
+// const batches = [
+//   {
+//     number: 1,
+//     startDate: 11-11-2011,
+//     endDate: 12-12-2011,
+//     students: [],
+//     totalScore: [],
+//   },
+//   {
+//     number: 2,
+//     startDate: 11-11-2012,
+//     endDate: 12-12-2012,
+//     students: [],
+//     totalScore: [],
+//   }
+// ]
