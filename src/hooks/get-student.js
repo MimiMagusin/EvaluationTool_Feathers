2d347e8fb4289345aa4e004e44d3b65e0f591582
MimiMagusin <mimi_magusin@hotmail.com>
2017-10-13 14:11:06 +0200

    Start Up Hook Service

--- src/hooks/get-student.js
+++ src/hooks/get-student.js
@@ -1,22 +1,32 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
-const rightStudent = require('./right-student');
 
-module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
-  return function createGame (hook) {
-    return rightStudent(hook)
-      .then((student) => {
+function randomIndex(max) {
+  return Math.floor(Math.random() * max);
+}
 
-        hook.data = {
-          firstName: hook.params.firstName,
-          lastName: hook.params.lastName,
-          profilePicture: hook.params.profilePicture,
-          batch: hook.params.batch,
-          currentEvaluation: hook.params.currentEvaluation,
-        };
-
-        return Promise.resolve(hook);
+module.exports = function(hook) {
+  if (!hook.id) {
+    return hook.app.service('students').find()
+      .then((result) => {
+        const allStudents = result.data;
+        return allStudents[randomIndex(allStudents.length - 1)];
       });
-  };
+  }
+
+  // return hook.app.service('games').get(hook.id)
+  //   .then((game) => {
+  //     return hook.app.service('riddles').find({
+  //       query: {
+  //         riddle: {
+  //           $nin: game.riddles.riddle
+  //         }
+  //       }
+  //     })
+  //       .then((result) => {
+  //         const all = result.data.map((riddle) => riddle._id);
+  //         return all[randomIndex(all.length - 1)];
+  //       });
+  //   });
 };
